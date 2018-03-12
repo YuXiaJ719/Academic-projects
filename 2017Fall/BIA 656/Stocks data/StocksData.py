d2e604f47f9ac32e71352f47284baeee5d859d4c
Yu Xia <35589010+YuXiaJ719@users.noreply.github.com>
2018-03-12 18:49:45 -0400

    Update StocksData.py

--- 2017Fall/BIA 656/Stocks data/StocksData.py
+++ 2017Fall/BIA 656/Stocks data/StocksData.py
@@ -7,7 +7,7 @@
 
 import time
 import pickle
-import pandas as pd
+import os
 import pandas_datareader as reader
 from datetime import datetime
 
@@ -28,23 +28,23 @@ def retrive_data(stock_name, start, end, source="yahoo"):
 
 
 if __name__ == "__main__":    
-   with open('s&p500tickerscode.pickle', 'rb') as f:
-       tickers = pickle.load(f)
+    with open('s&p500tickerscode.pickle', 'rb') as f:
+        tickers = pickle.load(f)
 
-   # Collect data from 01/01/2000 to now
-   start = datetime(2000, 1, 1)
-   end = datetime.now().date()
-   
-   # Initialize a dictionary to store stock data
+    #Collect data from 01/01/2000 to now
+    start = datetime(2000, 1, 1)
+    end = datetime.now().date()
+    
+   #Initialize a dictionary to store stock data
     stocks_data = {}
     
-   # Collect stock data and save as .csv file
-   for stock_name in tickers:
-       if(os.path.exists('./Data/' + stock_name + '.csv') != True):
-           data = retrive_data(stock_name, start, end)
-           data.to_csv('./Data/' + stock_name + '.csv')
-           stocks_data[stock_name] = data    
-        
-    # Save all stocks data in one file for easilt access
+    #Collect stock data and save as .csv file
+    for stock_name in tickers:
+        if(os.path.exists('./Data/' + stock_name + '.csv') != True):
+            data = retrive_data(stock_name, start, end)
+            data.to_csv('./Data/' + stock_name + '.csv')
+            stocks_data[stock_name] = data
+    
+    #Save all stocks data in one file for easilt access
     with open('StocksData.pickle', 'wb') as f:
         pickle.dump(stocks_data, f)
